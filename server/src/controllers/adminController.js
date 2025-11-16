ffeae1c6a5a71b7196169de2633fa0e007a1729f
AjayAR <ajayperies10@gmail.com>
2025-11-16 22:35:22 +0530

    Update adminController.js

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -1,1 +1,13 @@
 
+import Admin from "../models/adminModel.js";
+
+// Get overall platform stats
+export const getPlatformStats = async (req, res) => {
+  try {
+    const stats = await Admin.getPlatformStats();
+    res.json(stats);
+  } catch (error) {
+    console.error("Error getting platform stats:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
