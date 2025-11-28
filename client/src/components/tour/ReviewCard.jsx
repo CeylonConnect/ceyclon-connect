19edd9c9724a917156d25eb402b351554d740313
AjayAR <ajayperies10@gmail.com>
2025-11-28 22:57:06 +0530

    Add review text display to ReviewCard component

--- client/src/components/tour/ReviewCard.jsx
+++ client/src/components/tour/ReviewCard.jsx
@@ -15,3 +15,7 @@ export default function ReviewCard({ review }) {
               <path d="m12 17.27 6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
             </svg>
           ))}
+</div>
+        <p className="mt-2 text-[15px] text-neutral-700">{review.text}</p>
+      </div>
+    </div>
