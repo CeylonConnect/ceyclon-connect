d4d7a02e9c82a69091f7e4833c347e29503bfcc0
AjayAR <ajayperies10@gmail.com>
2025-11-28 22:55:40 +0530

    Add ReviewCard component for displaying reviews

--- client/src/components/tour/ReviewCard.jsx
+++ client/src/components/tour/ReviewCard.jsx
@@ -1,2 +1,7 @@
 import React from "react";
 
+export default function ReviewCard({ review }) {
+  return (
+    <div className="flex gap-4 rounded-2xl border border-neutral-200 bg-white p-4 shadow-sm">
+      <img src={review.avatar} alt={review.author} className="h-12 w-12 rounded-full object-cover" />
+      <div className="flex-1">
