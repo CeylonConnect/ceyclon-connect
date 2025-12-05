cfbf271a7602bf8d888d9c087dd517d2a7c4e576
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-06 01:39:40 +0530

    feat(home):  login, signup and dashboard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <>
+      <Navbar
+        isAuthenticated={false}
+        onLoginClick={() => navigate("/login")}
+        onSignupClick={() => navigate("/signup")}
+        onDashboardClick={() => navigate("/dashboard")}
+      />
       <main></main>
     </>
   );
