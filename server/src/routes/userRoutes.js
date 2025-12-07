13d490a020999a9d64a9f43633dbadd45827ab9d
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2025-12-07 08:23:41 +0530

    Add user registration and login routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/userController.js";
+
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
