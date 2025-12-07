4bb44ac492b85b13fe59c14453660c3d2b34f664
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2025-12-07 08:06:47 +0530

    update

--- server/src/routes/badgeRequestRoutes.js
+++ server/src/routes/badgeRequestRoutes.js
@@ -21,7 +21,6 @@ router.post("/", createBadgeRequest);
 // Get a badge request by ID
 router.get("/:id", getBadgeRequestById);
 
-
 // Get all badge requests (admin) with optional status filter
 router.get("/", getAllBadgeRequests);
 
