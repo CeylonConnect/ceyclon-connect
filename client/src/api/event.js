c13cc39c89128828b3d51f4d74444ffc146dc9cb
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 00:57:40 +0530

    feat: Add updateEvent API function

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -3,3 +3,4 @@ import { api } from "./client";
 export const createEvent = (data) => api.post("/events", data);
 export const getAllEvents = () => api.get("/events");
 export const getEventById = (id) => api.get(`/events/${id}`);
+export const updateEvent = (id, data) => api.put(`/events/${id}`, data);
