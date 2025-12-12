c5cfca0baa876a13b695662b004c128c455916ac
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 00:57:57 +0530

    feat: Add deleteEvent API function

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -4,3 +4,4 @@ export const createEvent = (data) => api.post("/events", data);
 export const getAllEvents = () => api.get("/events");
 export const getEventById = (id) => api.get(`/events/${id}`);
 export const updateEvent = (id, data) => api.put(`/events/${id}`, data);
+export const deleteEvent = (id) => api.del(`/events/${id}`);
