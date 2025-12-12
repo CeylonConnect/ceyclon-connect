fd9383157ca07b9cb394ccada71ead75f27657d3
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 00:57:20 +0530

    feat: Add getEventById API function

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -2,3 +2,4 @@ import { api } from "./client";
 
 export const createEvent = (data) => api.post("/events", data);
 export const getAllEvents = () => api.get("/events");
+export const getEventById = (id) => api.get(`/events/${id}`);
