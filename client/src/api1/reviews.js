d68d304db5c884a9be4ba1745218b7f04f30aca2
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 00:54:18 +0530

    feat: Add getGuideAverageRating API function

--- client/src/api1/reviews.js
+++ client/src/api1/reviews.js
@@ -4,3 +4,6 @@ export const getReviewsByGuide = async (guideId) => {
   const res = await api.get(`/reviews/guide/${encodeURIComponent(guideId)}`);
   return normalizeList(res);
 };
+
+export const getGuideAverageRating = (guideId) =>
+  api.get(`/reviews/guide/${encodeURIComponent(guideId)}/average`);
