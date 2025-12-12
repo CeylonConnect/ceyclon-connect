556cc3c1299f50efd42116b533cb6053cb04ee5f
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 01:10:59 +0530

    feat: Add getGuideAverageRating and getReviewsByTour functions

--- client/src/api1/messages.js
+++ client/src/api1/messages.js
@@ -4,3 +4,11 @@ export const getReviewsByGuide = async (guideId) => {
   const res = await api.get(`/reviews/guide/${encodeURIComponent(guideId)}`);
   return normalizeList(res);
 };
+
+export const getGuideAverageRating = (guideId) =>
+  api.get(`/reviews/guide/${encodeURIComponent(guideId)}/average`);
+
+export const getReviewsByTour = async (tourId) => {
+  const res = await api.get(`/reviews/tour/${encodeURIComponent(tourId)}`);
+  return normalizeList(res);
+};
