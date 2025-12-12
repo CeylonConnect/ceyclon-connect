b6b29bb82ee1662b57513bd24d43aa9d0228995a
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 00:54:35 +0530

    feat: Add getReviewsByTour API function

--- client/src/api1/reviews.js
+++ client/src/api1/reviews.js
@@ -7,3 +7,8 @@ export const getReviewsByGuide = async (guideId) => {
 
 export const getGuideAverageRating = (guideId) =>
   api.get(`/reviews/guide/${encodeURIComponent(guideId)}/average`);
+
+export const getReviewsByTour = async (tourId) => {
+  const res = await api.get(`/reviews/tour/${encodeURIComponent(tourId)}`);
+  return normalizeList(res);
+};
