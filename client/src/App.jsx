633b924ce893020268123fe20cb3febf23feb173
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-13 11:02:15 +0530

    refactor: Clean up App component by removing unused imports and restructuring routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import AllTours from "./pages/AllTours";
-import TourView from "./pages/tourview";
-import Login from "./pages/Login";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
+import Tours from "./pages/tour.jsx";
+import TourDetails from "./pages/TourDetails.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Event from "./pages/Events.jsx";
+import About from "./pages/about.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import LocalDashboard from "./pages/LocalDashboard.jsx";
+import { AuthProvider } from "./state/AuthContext";
+import { BookingProvider } from "./state/BookingContext";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home />} />
-  <Route path="/tours" element={<AllTours />} />
-  <Route path="/tours/:id" element={<TourView />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <BookingProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/tours/:slug" element={<TourDetails />} />
+
+          <Route path="/events" element={<Event />} />
+          <Route path="/about" element={<About />} />
+
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/local" element={<LocalDashboard />} />
+
+          <Route path="*" element={<div className="p-8 text-center">Page not found</div>} />
+        </Routes>
+      </BookingProvider>
+    </AuthProvider>
   );
 }
-
-export default App;
