9285b9cbcffae4f3897e566f45706c86fffb9964
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-15 23:34:01 +0530

    feat: Update main.jsx to use createRoot and add ToastContainer for notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover={false}
+        draggable
+        theme="colored"
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
