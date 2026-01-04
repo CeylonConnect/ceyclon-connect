a92cf451eb24254d0cec2ec7a5ac48c80fbc4694
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-01-04 22:42:54 +0530

    update

--- server/src/utils/validators.js
+++ server/src/utils/validators.js
@@ -8,6 +8,7 @@
  * @param {string} email - Email address
  * @returns {boolean} True if valid
  */
+
 export function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
