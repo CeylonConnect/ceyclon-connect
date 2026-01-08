5e16c9d3de06648a17d9f4abebe400a958e0e54c
AjayAR <ajayperies10@gmail.com>
2026-01-08 20:13:17 +0530

    Add function to get Cloudinary upload signature

--- client/src/api1/uploads.js
+++ client/src/api1/uploads.js
@@ -1,1 +1,7 @@
+import { api } from "./client";
+
+export const getCloudinarySignature = async (folder = "tours") => {
+  const q = folder ? `?folder=${encodeURIComponent(folder)}` : "";
+  return api.get(`/uploads/cloudinary-signature${q}`);
+};
 
