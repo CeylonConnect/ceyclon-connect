836066aceb2b7f85a126cb9f912895327e4824d3
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2026-01-15 21:36:18 +0530

    Refactor eventRoutes.js

--- server/src/routes/eventRoutes.js
+++ server/src/routes/eventRoutes.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { createEvent, getEventById, getAllEvents, updateEvent, deleteEvent } from "../controllers/eventController.js";
+import {
+  createEvent,
+  getEventById,
+  getAllEvents,
+  updateEvent,
+  deleteEvent,
+} from "../controllers/eventController.js";
+import { protect, requireAdmin } from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createEvent);
+router.post("/", protect, requireAdmin, createEvent);
 router.get("/", getAllEvents);
 router.get("/:id", getEventById);
-router.put("/:id", updateEvent);
-router.delete("/:id", deleteEvent);
-
+router.put("/:id", protect, requireAdmin, updateEvent);
+router.delete("/:id", protect, requireAdmin, deleteEvent);
 
 export default router;
